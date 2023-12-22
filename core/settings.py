b8a35af5385cc1fafcb7be61f8acbb10dd759c13
root <root@demeter.ciaer.adsl>
2023-12-22 10:01:25 -0300

    seti

--- core/settings.py
+++ core/settings.py
@@ -129,27 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+# Caminho para os arquivos estáticos coletados
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_ROOT = '/var/www/doc/staticfiles/'
 
-
-
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, "static"),
-) 
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     r"C:\Users\suporte\OneDrive\_dev\bd_cert",
-#     # r"C:\Users\suporte\OneDrive\_dev\sn\src",
-#     # r"C:\Users\suporte\OneDrive\_dev\bd_cert",
-
-# ]
-
+# Caminho para os arquivos de mídia
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/doc/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
